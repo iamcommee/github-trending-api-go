9ab17f2a8f37d0cfd2d9da03e1820c4ef84e01ee
iamcommee <i.am.commee@gmail.com>
2022-09-25 12:28:40 +0700

    Adjust codes

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -14,9 +17,14 @@ type Repos struct {
 }
 
 type Repo struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
+	Owner          string `json:"owner"`
+	RepositoryName string `json:"repository_name"`
+	Repository     string `json:"repository "`
+	Description    string `json:"description"`
+	URL            string `json:"url"`
+	Star           int    `json:"star"`
+	Fork           int    `json:"fork"`
+	TimeFrameStar  int    `json:"time_frame_star"`
 }
 
 func main() {
@@ -49,17 +57,59 @@ func getRepos(language, timeFrame string) Repos {
 
 	var Repos Repos
 	c.OnHTML(".Box-row", func(e *colly.HTMLElement) {
-		name := e.ChildAttr(".lh-condensed a", "href")
-		name, _ = url.PathUnescape(name)
-		name = strings.Replace(name, "/", "", 1)
+
+		repoHtmlAttr := e.ChildAttr(".lh-condensed a", "href")
+		repoHtmlValue, _ := url.PathUnescape(repoHtmlAttr)
+
+		// 0 = white space
+		// 1 = owner
+		// 2 = repository name
+		repoSplitter := strings.Split(repoHtmlValue, "/")
+
+		owner := repoSplitter[1]
+		repoName := repoSplitter[2]
+		repo := owner + "/" + repoName
+
+		starHtmlAttr := fmt.Sprintf(`a[href='%s']`, "/"+repo+"/stargazers")
+		forkHtmlAttr := fmt.Sprintf(`a[href='%s']`, "/"+repo+"/network/members"+"."+repoName)
 
 		description := e.ChildText("p")
 
+		starHtmlValue := e.ChildText(starHtmlAttr)
+		star, err := strconv.Atoi(strings.Replace(starHtmlValue, ",", "", -1))
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		forkHtmlValue := e.ChildText(forkHtmlAttr)
+		fork, err := strconv.Atoi(strings.Replace(forkHtmlValue, ",", "", -1))
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		timeFrameStarHtmlValue := e.ChildText("span[class='d-inline-block float-sm-right']")
+
+		timeFrameStarSplitter := strings.Split(timeFrameStarHtmlValue, " ")
+
+		timeFrameStar, err := strconv.Atoi(strings.Replace(timeFrameStarSplitter[0], ",", "", -1))
+
+		if err != nil {
+			log.Println(err)
+		}
+
 		Repos.Repos = append(Repos.Repos, Repo{
-			Name:        name,
-			Description: description,
-			URL:         "https://github.com/" + name,
+			Owner:          owner,
+			RepositoryName: repoName,
+			Repository:     repo,
+			Description:    description,
+			URL:            "https://github.com/" + repo,
+			Star:           star,
+			Fork:           fork,
+			TimeFrameStar:  timeFrameStar,
 		})
+
 	})
 
 	c.Visit("https://github.com/trending/" + language + "?since=" + timeFrame)
